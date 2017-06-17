eb85682d39a109708fe6893e993ed376a4dbc0f7
EmerickH <emerick_herve@orange.fr>
2017-06-17 14:20:13 +0200

    Add listdevices.sh for Pyaudio

---
+++ listdevices.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+echo "Audio devices:"
+python << END
+import pyaudio
+
+p = pyaudio.PyAudio()
+for i in range(p.get_device_count()):
+	print(p.get_device_info_by_index(i).get('name'))
+END
